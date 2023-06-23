d70af43b1632a2063777ce7722fe09eff22c58c7
BawoolJo <bawool1010@daum.net>
2023-06-24 00:33:42 +0900

    #6- 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    초기 설정:
    
    * 스프링 웹
    * 액츄에이터
    *데브툴즈
    *롬복

---
+++ exercise-board/src/test/java/com/exercise/board/ExerciseBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
