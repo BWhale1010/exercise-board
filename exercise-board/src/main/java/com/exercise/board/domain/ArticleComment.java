67106e96acbdf98f5386fab78984d967d8cb3c34
BawoolJo <bawool1010@daum.net>
2023-06-24 22:27:07 +0900

    #10 - 도메인 코드를 엔티티로 변환
    1 도메인 클래스 == 1테이블 매칭되도록 설계
    생성자와 `equals()`,`hashcode()` 등이 
    도메인 맥락에 맞게끔 디테일 하게 설정

--- exercise-board/src/main/java/com/exercise/board/domain/ArticleComment.java
+++ exercise-board/src/main/java/com/exercise/board/domain/ArticleComment.java
@@ -1,17 +1,64 @@
 package com.exercise.board.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createBy"),
+
+})
+@Entity
 public class ArticleComment {
-    private Long id;
-    private Article article; // 게시글 (ID)
-    private User_account user_account; // 유저 계정 (아이디)
-    private long parent_comment_id; // 부모 댓글
-    private String content; // 본문
-    
 
-    private LocalDateTime createdAt; // 생성일시
-    private String createBy; // 생성자
-    private  LocalDateTime modifiedAt; // 수정일시
-    private String modifiedBy; // 수정자
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter @ManyToOne(optional = false) private Article article; // 게시글 (ID)
+    // private User_account user_account; // 유저 계정 (아이디)
+    // private long parent_comment_id; // 부모 댓글
+    @Setter @Column(nullable = false, length = 500) private String content; // 본문
+
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
+    @CreatedBy @Column(nullable = false, length = 100) private String createBy; // 생성자
+    @LastModifiedDate @Column(nullable = false) private  LocalDateTime modifiedAt; // 수정일
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
+
+    protected ArticleComment() {
+    }
+
+    private ArticleComment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    public ArticleComment of(Article article, String content) {
+        return new ArticleComment(article,content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleComment that)) return false;
+        return id != null && id.equals(id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
