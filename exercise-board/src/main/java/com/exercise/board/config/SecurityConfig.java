e21a3a28dc9a63c2633fc5b582219f0067524e0c
BawoolJo <bawool1010@daum.net>
2023-06-27 17:36:39 +0900

    #15 - 시큐리티 설정을 모든 요청에 인증이 열리도록 수정
    테스트 단계에서 인증 기능 구현 단계 전 
    모든 인증 설정을 오픈 
    form 로그인 활성화해서 로그인 뷰가 그려지게 함
    최신 스프링 시큐리티 인증 방법을 적용
    
    * spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter

--- exercise-board/src/main/java/com/exercise/board/config/SecurityConfig.java
+++ exercise-board/src/main/java/com/exercise/board/config/SecurityConfig.java
@@ -1,16 +1,23 @@
 package com.exercise.board.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig{
 
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.httpBasic().disable();
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       return http
+                .authorizeHttpRequests(auth->auth.anyRequest().permitAll())
+                .formLogin().and()
+                .build();
+
+
+
     }
 }
